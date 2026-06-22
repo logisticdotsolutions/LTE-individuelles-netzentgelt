9886f06f996fffebf8ebe0d65833985cb4172018
railverk <orglchristoph@gmail.com>
2026-06-22 08:53:07 +0200

    feat: register correction rebuild mode

--- scripts/pipeline/rebuild_modes.py
+++ scripts/pipeline/rebuild_modes.py
@@ -5,6 +5,7 @@ class RebuildMode(str, Enum):
     FULL_IMPORT_REBUILD = "FULL_IMPORT_REBUILD"
     RAW_IMPORT_REBUILD = "RAW_IMPORT_REBUILD"
     FULL_REBUILD_FROM_RAW = "FULL_REBUILD_FROM_RAW"
+    CORRECTION_REBUILD = "CORRECTION_REBUILD"
     OVERRIDE_REBUILD = "OVERRIDE_REBUILD"
     EXPORT_REBUILD = "EXPORT_REBUILD"
 
@@ -14,6 +15,7 @@ class RebuildMode(str, Enum):
             RebuildMode.FULL_IMPORT_REBUILD,
             RebuildMode.RAW_IMPORT_REBUILD,
             RebuildMode.FULL_REBUILD_FROM_RAW,
+            RebuildMode.CORRECTION_REBUILD,
             RebuildMode.OVERRIDE_REBUILD,
             RebuildMode.EXPORT_REBUILD,
         }
